47dd84e5a9a60cd2d01440901834d9c02be2cc11
mustafaalhaj <164753938+mustafaalhaj@users.noreply.github.com>
2025-12-23 23:49:18 +0100

    Delete update_sitemap.ps1

--- update_sitemap.ps1
+++
@@ -1,7 +0,0 @@
-$snippet = Get-Content -Raw sitemap_snippet.txt
-$xml = Get-Content -Raw sitemap.xml
-# Remove any trailing newlines from snippet to keep it clean
-$snippet = $snippet.Trim()
-# Replace closing tag with snippet + closing tag
-$xml = $xml -replace '</urlset>', "$snippet`n</urlset>"
-$xml | Set-Content sitemap.xml -Encoding UTF8
